0a0d018ccc8fd5442c06833d77e3f8a1de788139
Majed Shrendan <majedshrndan45@gmail.com>
2019-01-31 16:19:28 +0100

    added node_modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-((args) => {
+import namedColors from "color-name-list";
+import nearestColors from "nearest-color";
+(args => {
   // All code goes inside here
-  
+
+  args.forEach(arg => {
+    const colors = namedColors.find(color => color.hex === arg);
+
+    const nearColor = namedColors.reduce(
+      (o, { name, hex }) => Object.assign(o, { [name]: hex }),
+      {}
+    );
+
+    const nearest = nearestColors.from(nearColor);
+
+    if (colors) {
+      console.log(`your color is  ${colors.name}`);
+    } else {
+      console.log(
+        `the nearColor is ${nearest(arg).value} for the color  is ${
+          nearest(arg).name
+        }`
+      );
+      // console.error(`${arg} this color is not exsit `);
+    }
+  });
 })(process.argv.slice(2));
